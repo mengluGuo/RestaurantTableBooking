6ae98182b978defee39bf5b1630761a7a8b521f3
MengluGuo <gml1988109@gmail.com>
2018-04-09 13:25:22 +0100

    fix bugs

--- myJS/enter_details.js
+++ myJS/enter_details.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
+
     $('#submit').bind('click', function () {
+        // $('#signInForm').formValidation();
+
         var input_title = $('#title option:selected').text();
         var input_first_name = $('#first-name').val();
         var input_last_name = $('#last-name').val();
         var input_email = $('#email').val();
         var input_phone = $('#phone').val();
-        var person_info = {title: input_title, first_name: input_first_name, last_name: input_last_name, email: input_email, phone: input_phone};
+        var person_info = {title:input_title, first_name:input_first_name, last_name:input_last_name, email:input_email, phone:input_phone};
 
+        // alert('111');
         // alert(JSON.stringify(person_info));
         localStorage.setItem('person_info', JSON.stringify(person_info));
+        // alert(localStorage.getItem('person_info'));
     })
 });
